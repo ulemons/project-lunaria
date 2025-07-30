b4b5de9ff227941156cb394e4909301cf31cd719
ulemons <ulemons92@gmail.com>
2025-07-30 16:33:20 +0200

    feat: first commit

---
+++ postinstall-warning.js
@@ -0,0 +1,9 @@
+
+console.warn(`\u001b[33m[⚠️  Project Lunaria Warning]\u001b[0m
+Make sure you have installed libcamera-still on your Raspberry Pi.
+Use the command below if you haven't:
+
+  \u001b[36msudo apt install libcamera-apps\u001b[0m
+
+This package does NOT auto-install system dependencies.
+`);
