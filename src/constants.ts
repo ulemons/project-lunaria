c7647fee3e0269d9669f53b8c30d089cddbcc301
ulemons <ulemons92@gmail.com>
2025-08-01 15:00:53 +0200

    feat: add cron constants

---
+++ src/constants.ts
@@ -0,0 +1,5 @@
+export const CRON_5_MINUTES = '*/5 * * * *';
+export const CRON_10_MINUTES = '*/10 * * * *';
+export const CRON_15_MINUTES = '*/15 * * * *';
+export const CRON_30_MINUTES = '*/30 * * * *';
+export const CRON_HOURLY = '0 * * * *';
--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,15 @@
 import { CronJob } from 'cron';
 import { ensurePhotosDir, takePhoto } from './camera';
 import { startApiServer } from './api';
+import { CRON_5_MINUTES } from './constants';
 
 export function startLunaria(): void {
   ensurePhotosDir();
 
   // Run every hour on the hour
-  const job = new CronJob('0 0 * * * *', takePhoto, null, true, 'UTC');
+  const job = new CronJob(CRON_5_MINUTES, takePhoto, null, true, 'UTC');
 
-  console.log('[🌱] Project Lunaria started. Taking photos every hour.');
+  console.log('[🌱] Project Lunaria started. Taking photos of your plants.');
 }
 
 // Start API and Lunaria when run directly
