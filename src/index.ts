3ea25f054c61fe995952d636f69171d45c0c7765
Umberto Sgueglia <ulemons92@gmail.com>
2025-09-29 17:50:00 +0200

    set hourly

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,13 @@
 import { CronJob } from 'cron';
 import { ensurePhotosDir, takePhoto } from './camera';
 import { startApiServer } from './api';
-import { CRON_5_MINUTES } from './constants';
+import { CRON_HOURLY } from './constants';
 
 export function startLunaria(): void {
   ensurePhotosDir();
 
   // Run every hour on the hour
-  const job = new CronJob(CRON_5_MINUTES, takePhoto, null, true, 'UTC');
+  const job = new CronJob(CRON_HOURLY, takePhoto, null, true, 'UTC');
 
   console.log('[🌱] Project Lunaria started. Taking photos of your plants.');
 }
